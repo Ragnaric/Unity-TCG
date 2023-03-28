a29b0589ddd3c956c7654fa5ec028934b88506d8
Jonathan <navakino@gmail.com>
2023-03-28 13:33:18 -0500

    Implement script to display card stats.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Card : MonoBehaviour
+{
+    public int attackPower, currentHealth, manaCost;
+
+    public TMP_Text attackText, defText, manaText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackText.text = attackPower.ToString();
+        defText.text = currentHealth.ToString();
+        manaText.text = manaCost.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
