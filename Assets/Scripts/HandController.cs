0b56f62795b1a443247f5a4e4e1c40b36d8b0bb4
Jonathan <navakino@gmail.com>
2023-03-28 20:37:17 -0500

    Implement logic to place cards in hand area.

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class HandController : MonoBehaviour
 {
-    public Card[] heldCards;
+    public List<Card> heldCards = new List<Card>();
     public Transform minPos, maxPos;
+    public List<Vector3> cardPositions = new List<Vector3>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,18 @@ public class HandController : MonoBehaviour
 
     public void SetCardPositionsHand()
     {
+        cardPositions.Clear();
 
+        Vector3 distanceBetweenPoints = Vector3.zero;
+        if (heldCards.Count > 1)
+        {
+            distanceBetweenPoints = (maxPos.position - minPos.position) / (heldCards.Count - 1);
+        }
+
+        for (int i = 0; i < heldCards.Count; i++)
+        {
+            cardPositions.Add(minPos.position + (distanceBetweenPoints * i));
+            heldCards[i].transform.position = cardPositions[i];
+        }
     }
 }
