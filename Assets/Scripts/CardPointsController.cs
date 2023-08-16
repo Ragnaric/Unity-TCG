33c3d766c00ed75b727dcf728f892b1c6fa436b0
Jonathan <navakino@gmail.com>
2023-08-16 12:22:31 -0500

    Fix issue with card point controller script where the array was not referring to the card place points class; add cards to the array.

--- Assets/Scripts/CardPointsController.cs
+++ Assets/Scripts/CardPointsController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CardPointsController : MonoBehaviour
 {
+    public static CardPointsController instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public CardPlacePoint[] playerCardPoints, enemyCardPoints;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
