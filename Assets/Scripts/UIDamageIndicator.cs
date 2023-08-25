f9f2afc8a01940592ce9b68127e5e12b224f06a5
Jonathan Navarrete <navakino@gmail.com>
2023-08-25 14:34:15 -0500

    Add functionality for the damage indicator to descend slowly on the UI

--- Assets/Scripts/UIDamageIndicator.cs
+++ Assets/Scripts/UIDamageIndicator.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIDamageIndicator : MonoBehaviour
 {
+    public TMP_Text damageTaken;
+
+    public float moveSpeed, displayTime = 3f;
+
+    private RectTransform canvas;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, displayTime);
+        canvas = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        canvas.anchoredPosition += new Vector2(0, -moveSpeed * Time.deltaTime);
     }
 }
